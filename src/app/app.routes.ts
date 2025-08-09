77b1011fcf6c7b5f76a21b4346bb0d1fc42b8d28
Arnab Khan <aranabkhan@gmail.com>
2025-08-09 21:19:17 +0530

    router guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '../shared/guards/auth.guard';
+import { GuestGuard } from '../shared/guards/guest.guard';
 
 export const routes: Routes = [
     {
@@ -8,11 +10,13 @@ export const routes: Routes = [
     },
     {
         path: 'sign-up',
-        loadComponent: () => import('./pages/sign-up-page/sign-up-page.component').then(m => m.SignUpPageComponent)
+        loadComponent: () => import('./pages/sign-up-page/sign-up-page.component').then(m => m.SignUpPageComponent),
+        canActivate: [GuestGuard]
     },
     {
         path: 'login',
-        loadComponent: () => import('./pages/login-page/login-page.component').then(m => m.LoginPageComponent)
+        loadComponent: () => import('./pages/login-page/login-page.component').then(m => m.LoginPageComponent),
+        canActivate: [GuestGuard]
     },
     {
         path: 'blogs',
@@ -20,6 +24,7 @@ export const routes: Routes = [
     },
     {
         path: 'edit-profile',
-        loadComponent: () => import('./pages/edit-profile-page/edit-profile-page.component').then(m => m.EditProfilePageComponent)
+        loadComponent: () => import('./pages/edit-profile-page/edit-profile-page.component').then(m => m.EditProfilePageComponent),
+        canActivate: [AuthGuard]
     },
 ];
---
+++ src/shared/guards/guest.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map, filter, take } from 'rxjs/operators';
+import { userSelector } from '../ngrx/ngrx.selector';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestGuard implements CanActivate {
+  private store = inject(Store);
+  private router = inject(Router);
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.checkAccess();
+  }
+
+  private checkAccess(): Observable<boolean> {
+
+    return this.store.select(userSelector).pipe(
+      filter(user => user._id !== undefined),
+      take(1),
+      map(user => {
+        if (user?._id) {
+          this.router.navigate(['/blogs']);
+          return false;
+        }
+        return true;
+      })
+    );
+  }
+}
